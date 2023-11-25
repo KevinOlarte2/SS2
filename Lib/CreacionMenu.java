3e472c5b22134786c55d935bb9331ce6014ac118
kevinOlarte1 <kevinolarte.ko@gmail.com>
2023-11-25 17:44:54 +0100

    N

--- Lib/CreacionMenu.java
+++ Lib/CreacionMenu.java
@@ -1,1 +1,11 @@
-package Lib;
+
+public class CreacionMenu {
+
+    public static void main(String[] args) {
+        System.out.println("Hola com estas");
+    }
+
+    public static String aaa (String oa){
+        return oa.toLowerCase();
+    }
+}
